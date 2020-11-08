fe9ef93d5daf9f4a94dc2461eebad389e42bd3d0
i1620 <68018020+i1620@users.noreply.github.com>
2020-11-08 16:46:38 -0600

    added new code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,38 +1,96 @@
 // @TODO: YOUR CODE HERE!
-const svgWidth = 960;
-const svgHeight = 500;
+function makeResponsive() {
 
-const margin = {
-        top: 20,
-        right: 40,
-        bottom: 80,
-        left: 100
-    };
+    let svgWidth = 660;
+    let svgHeight = 460;
 
-// Calculating chart width and height
-const width = svgWidth - margin.left - margin.right;
-const height = svgHeight - margin.top - margin.bottom;
+    let margin = {
+            top: 20,
+            right: 40,
+            bottom: 80,
+            left: 100
+        };
 
-// SVG wrapper
-const svg = d3.select("#scatter")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    // Calculating chart width and height
+    let width = svgWidth - margin.left - margin.right;
+    let height = svgHeight - margin.top - margin.bottom;
 
-// Append SVG group
-const chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})` )
+    // SVG wrapper
+    let svg = d3.select("#scatter")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
 
-// Initial params
-var chosenXaxis = "poverty";
-var chosenYaxis = "healthcare";
+    // Append SVG
+    let chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})` )
 
-function renderXAxes(newXScale, XAxis) {
-    const bottomAxis = d3.axisBottom(newXScale);
+    // Import data from csv file
+    d3.csv("assets/data/data.csv")
+        .then(function (riskData) {
 
-    XAxis.transition()
-        .duration(1000)
-        .call(bottomAxis);
+            // Get data from csv file
+            riskData.forEach(function (data) {
+                data.age = +data.age;
+                data.smokes = +data.smokes;
+                data.healthcare = +data.healthcare;
+                data.poverty = +data.poverty;
+                data.abbr = data.abbr;
+                data.income = +data.income;
+            });
+            
+            // Scales for X and Y
+            let xLinearScale = d3.scaleLinear()
+                .domain([8.5, d3.max(riskData, d=> d.poverty)])
+                .range([0,width]);
+            
+            let yLinearScale = d3.scaleLinear()
+                .domain([3.5, d3.max(riskData, d=>d.healthcare)])
+                .range([height,0]);
+            
+            // Creating the axis
+            let xAxis =d3.axisBottom(xLinearScale);
+            let yAxis = d3.axisLeft(yLinearScale);
 
-    return XAxis;
+            // Append axis to chartGroup
+            chartGroup.append("g")
+                .attr("transform", `translate(0, ${height})`)
+                .call(xAxis);
+            
+            chartGroup.append("g")
+                .call(yAxis);
+
+            // Make circles
+            let circlesGroup = chartGroup.selectAll("circle")
+                .data(riskData)
+                .enter()
+                .append("circle")
+                .attr("cx", d => xLinearScale(d.poverty))
+                .attr("cy", d => yLinearScale(d.healthcare))
+                .attr("r",10)
+                .attr("fill","blue")
+                .attr("opacity",".6")
+                .attr("stroke-width","1")
+                .attr("stroke","black");
+            
+                // States names
+            chartGroup.select("g")
+                .selectAll("circle")
+                .data(riskData)
+                .enter()
+                .append("text")
+                .text(d => d.abbr)
+                .attr("x", d => xLinearScale(d.poverty))
+                .attr("y", d => yLinearScale(d.healthcare))
+                .attr("dy", -400)
+                .attr("text-anchor", "middle")
+                .attr("font-size", "12px")
+                .attr("fill", "red");
+
+            console.log(riskData);
+
+            
+        });
 }
+
+makeResponsive();
